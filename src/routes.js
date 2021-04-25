51f95c95c84f3463d00086d6ccd731b244df17f3
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-04-24 22:41:47 -0300

    aula 2

--- src/routes.js
+++ src/routes.js
@@ -30,12 +30,28 @@ const Profile = {
         update(req, res) {
             // req.body para pegar os dados
             const data = req.body
+
             // Definir quantas semanas tem um ano: 52
             const weeksPerYear = 52
+
             // Remover as semanas de férias do ano, para pegar quantas semanas tem em 1 mês
             const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+
             // Total de hroas trabalhadas na semana
-            const weeksTotalHours = data["hours-per-day"] * data["days-per-week"]
+            const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+
+            // Horas trabalhadas no mês
+            const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+            // Valor da hora trabalhada
+            const valueHour = data["monthly-budget"] / monthlyTotalHours
+
+            Profile.data = {
+                ...Profile.data,
+                ...req.body,
+                "value-hour": valueHour,
+            }
+            return res.redirect('/profile')
         }
     }
 }
@@ -92,6 +108,10 @@ const Job = {
                 created_at: Date.now() // Atruibuindo a data de hoje
             })
             return res.redirect('/')
+        },
+
+        show(req, res) {
+            
         }
     },
 
